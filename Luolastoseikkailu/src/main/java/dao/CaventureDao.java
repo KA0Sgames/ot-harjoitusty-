85d727e550fe0de65abb2f14f24b8c3303dfa777
Joonas Ylönen <KA0Screativity@gmail.com>
2020-11-26 23:21:16 +0200

    Delete unnecessary class Container and tests for it. Write methods for manipulating character info in database to CaventureDao.java

--- Luolastoseikkailu/src/main/java/dao/CaventureDao.java
+++ Luolastoseikkailu/src/main/java/dao/CaventureDao.java
@@ -1,6 +1,8 @@
 package dao;
 
 import java.sql.*;
+import java.util.ArrayDeque;
+import domain.CharacterInfo;
 
 public class CaventureDao {
     
@@ -66,6 +68,104 @@ public class CaventureDao {
         return pass.equals(resultPassword);
     }
     
+    private int getUserId(String user) throws SQLException {
+        if (!containsUser(user)) {
+            return -1;
+        }
+        
+        Connection db = createConnection();
+        
+        PreparedStatement p = db.prepareStatement("SELECT Users.id FROM Users WHERE Users.username=?");
+        p.setString(1, user);
+        
+        ResultSet r = p.executeQuery();
+        
+        return r.getInt("id");
+    }
+    
+    public boolean addCharacter(String user, String name) throws SQLException {
+        if (containsCharacter(name)) {
+            return false;
+        }
+        
+        int UserId = getUserId(user);
+        
+        if (UserId == -1) {
+            return false;
+        }
+        
+        Connection db = createConnection();
+        
+        PreparedStatement addingCharacter = db.prepareStatement("INSERT INTO Characters (user_id, name) VALUES (?,?)");
+        addingCharacter.setInt(1, UserId);
+        addingCharacter.setString(2, name);
+        
+        addingCharacter.execute();
+        
+        db.close();
+        return true;
+    }
+    
+    public void updateCharacterExperience(String name, int experience) throws SQLException {
+        Connection db = createConnection();
+        
+        PreparedStatement updateCharacter = db.prepareStatement("UPDATE Characters SET Characters.experience=Characters.experience+? WHERE Characters.name=?");
+        updateCharacter.setInt(1, experience);
+        updateCharacter.setString(1, name);
+        
+        updateCharacter.executeUpdate();
+        
+        db.close();
+    }
+    
+    public void updateCharacterGold(String name, int gold) throws SQLException {
+        Connection db = createConnection();
+        
+        PreparedStatement updateCharacter = db.prepareStatement("UPDATE Characters SET Characters.gold=Characters.gold+? WHERE Characters.name=?");
+        updateCharacter.setInt(1, gold);
+        updateCharacter.setString(1, name);
+        
+        updateCharacter.executeUpdate();
+        
+        db.close();
+    }
+    
+    private boolean containsCharacter(String name) throws SQLException {
+        Connection db = createConnection();
+        
+        PreparedStatement p = db.prepareStatement("SELECT Characters.name FROM Characters WHERE Characters.name=?");
+        p.setString(1, name);
+        
+        ResultSet r = p.executeQuery();
+        
+        db.close();
+        return r.next();
+    }
+    
+    public ArrayDeque<CharacterInfo> getCharacters(String user) throws SQLException {
+        ArrayDeque<CharacterInfo> characters = new ArrayDeque<>();
+        
+        Connection db = createConnection();
+        
+        PreparedStatement getCharacters = db.prepareStatement("SELECT Characters.name, Characters.experience, Characters.gold FROM Users, Characters WHERE Users.id=Characters.user_id AND Users.name=? ORDER BY Characters.name");
+        getCharacters.setString(1, user);
+        
+        ResultSet r = getCharacters.executeQuery();
+        
+        while (r.next()) {
+            String name = r.getString("name");
+            int experience = r.getInt("experience");
+            int gold = r.getInt("gold");
+            
+            CharacterInfo character = new CharacterInfo(name, experience, gold);
+            
+            characters.addLast(character);
+        }
+        
+        db.close();
+        return characters;
+    }
+
     private Connection createConnection() throws SQLException {
         Connection db = DriverManager.getConnection(this.database);
         return db;
